e7e561b293aa2f3e1d9b8140b86470a31c1e97c5
Kristijonas92 <kristijonas.politika@gmail.com>
2023-02-12 20:08:12 +0200

    Registration

--- baigiamasis_darbas/src/components/Registration.jsx
+++ baigiamasis_darbas/src/components/Registration.jsx
@@ -3,22 +3,23 @@ import { useState, useContext } from 'react';
 import * as Yup from 'yup';
 import { Formik, Form, Field } from 'formik';
 import { Link, useNavigate } from 'react-router-dom';
-
+import GreatPosts from '../img/GreatPosts.png'
 
 const Registration = () => {
     const [error, setError] = useState('');
-    const { addUser } = useContext(UserContext);
+    const { users } = useContext(UserContext);
     const navigate = useNavigate();
 
     const validationSchema = Yup.object().shape({
         name: Yup.string().required('Name is required'),
         email: Yup.string().email('Invalid email').required('Email is required'),
         password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
+        passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Password confirmation is required'),
         photo: Yup.string().url('Invalid URL').required('Photo URL is required'),
     });
 
     const handleSubmit = (values, { setSubmitting }) => {
-        addUser(values)
+        users(values)
             .then(() => {
                 setSubmitting(false);
                 navigate('/');
@@ -31,26 +32,42 @@ const Registration = () => {
 
     return (
         <div>
+            <header>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                </nav>
+                <img src={GreatPosts} alt="Logo" />
+            </header>
             <h1>Registration</h1>
             {error && <p>{error}</p>}
             <Formik
-                initialValues={{ name: '', email: '', password: '', photo: ''}}
+                initialValues={{ name: '',
+                email: '',
+                password: '',
+                passwordConfirm: '',
+                photo_url: '' }}
                 validationSchema={validationSchema}
                 onSubmit={handleSubmit}
             >
-                {({ isSubmitting }) => (
-                    <Form>
+                
+                    <Form >
                         <Field type="text" name="name" placeholder="Name" />
                         <Field type="email" name="email" placeholder="Email" />
                         <Field type="password" name="password" placeholder="Password" />
-                        <Field type="text" name="photo" placeholder="Photo URL" />
-                        <button type="submit" disabled={isSubmitting}>
+                        <Field type="password" name="passwordConfirm" placeholder="Confirm Password" />
+                        <Field type="text" name="photo_url" placeholder="Photo URL" />
+                        <button type="submit">
                             Register
                         </button>
-                        <Link to="/">Cancel</Link>
+                        <Link to="/login">Cancel</Link>
                     </Form>
-                )}
             </Formik>
+
+            <footer>
+                <p>Copyright &copy; {new Date().getFullYear()}</p>
+            </footer>
         </div>
     );
 };
